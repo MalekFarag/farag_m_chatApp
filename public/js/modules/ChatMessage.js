2a568317203748de4060765927ebae4fa16db9b3
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-01-29 10:15:39 -0500

    basic messaging working (need to further id users)

--- public/js/modules/ChatMessage.js
+++ public/js/modules/ChatMessage.js
@@ -2,5 +2,17 @@
 // will be made public when you import this file into another via the import statement
 
 export default {
+    props: ['msg'],
+    template: `
+    <li class='messages'>
+    <p class='new=message'>
+        <span>{{msg.message.name}}:</span><br>
+        {{msg.message.content}}
+    </p>
+    </li>
+    `,
 
+    data: function(){
+        return { message: 'hello from template'};
+    }
 }
